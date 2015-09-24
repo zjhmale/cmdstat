c609cf1ff6fe8e18faced1dce91baccf2c3ccf2f
zjhmale <zjhmale@gmail.com>
2015-09-25 01:28:26 +0800

    init

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject zsh-history "a tool to find you top 10 command ever your used"
+  :description "FIXME: write description"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [environ "1.0.1"]]
+  :main ^:skip-aot zsh-history.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ test/zsh_history/core_test.clj
@@ -0,0 +1,7 @@
+(ns zsh-history.core-test
+  (:require [clojure.test :refer :all]
+            [zsh-history.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
