824d24248055d0b91ed31a0c428e6cbc870045ee
zjhmale <zjhmale@gmail.com>
2015-09-25 01:59:47 +0800

    大致功能完成

--- src/zsh_history/core.clj
+++ src/zsh_history/core.clj
@@ -1,7 +1,41 @@
 (ns zsh-history.core
+  (:require [environ.core :refer [env]]
+            [clojure.string :as cs])
   (:gen-class))
 
+(defn history-line->command
+  [line]
+  (-> line
+      (cs/split #";")
+      second))
+
+(defn weight [_ count] count)
+
+(defn sort-by-weight
+  [m]
+  (->> (vec m)
+       (sort-by (partial apply weight))
+       reverse))
+
+(defn format-history-map
+  [history-map]
+  (letfn [(format-one [[command weight]]
+                      (format "%s => %d times\n" command weight))]
+    (cs/join (map format-one history-map))))
+
+(defn format-history-lines
+  [history-lines]
+  (->> (map history-line->command history-lines)
+       (frequencies)
+       (sort-by-weight)
+       (take 10)
+       (format-history-map)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (-> (env :home)
+      (str "/.zsh_history")
+      slurp
+      (cs/split #"\n")
+      format-history-lines
+      print))
